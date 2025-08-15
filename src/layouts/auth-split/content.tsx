fd72c451b753106afea974ac52f01cd885106875
Dang Tho <dtho961@gmail.com>
2025-08-15 23:14:55 +0700

    start project

---
+++ src/layouts/auth-split/content.tsx
@@ -0,0 +1,60 @@
+import type { BoxProps } from '@mui/material/Box';
+import type { Breakpoint } from '@mui/material/styles';
+
+import { mergeClasses } from 'minimal-shared/utils';
+
+import Box from '@mui/material/Box';
+
+import { layoutClasses } from '../core/classes';
+import { CONFIG } from 'src/global-config';
+
+// ----------------------------------------------------------------------
+
+export type AuthSplitContentProps = BoxProps & { layoutQuery?: Breakpoint };
+
+export function AuthSplitContent({
+  sx,
+  children,
+  className,
+  layoutQuery = 'md',
+  ...other
+}: AuthSplitContentProps) {
+  return (
+    <Box
+      className={mergeClasses([layoutClasses.content, className])}
+      sx={[
+        (theme) => ({
+          backgroundImage: `url(${CONFIG.assetsDir}/assets/background/balloon.jpg)`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          display: 'flex',
+          flex: '1 1 auto',
+          alignItems: 'center',
+          flexDirection: 'column',
+          p: theme.spacing(3, 2, 10, 2),
+          [theme.breakpoints.up(layoutQuery)]: {
+            justifyContent: 'center',
+            p: theme.spacing(10, 2, 10, 2),
+          },
+        }),
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+      {...other}
+    >
+      <Box
+        sx={{
+          width: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          maxWidth: 'var(--layout-auth-content-width)',
+          padding: 4,
+          boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+          bgcolor: 'background.paper',
+          borderRadius: 2
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+}
---
+++ src/routes/sections/index.tsx
@@ -0,0 +1,45 @@
+import { Navigate, type RouteObject } from 'react-router';
+
+import { lazy, Suspense } from 'react';
+
+import { MainLayout } from 'src/layouts/main';
+
+import { SplashScreen } from 'src/components/loading-screen';
+
+import { authRoutes } from './auth';
+import { mainRoutes } from './main';
+import { authDemoRoutes } from './auth-demo';
+import { dashboardRoutes } from './dashboard';
+import { componentsRoutes } from './components';
+import { paths } from '../paths';
+
+// ----------------------------------------------------------------------
+
+const Page404 = lazy(() => import('src/pages/error/404'));
+
+export const routesSection: RouteObject[] = [
+  {
+    path: '/',
+    element: (
+      <Navigate
+        to={paths.dashboard.root}
+      />
+    ),
+  },
+
+  // Auth
+  ...authRoutes,
+  ...authDemoRoutes,
+
+  // Dashboard
+  ...dashboardRoutes,
+
+  // Main
+  ...mainRoutes,
+
+  // Components
+  ...componentsRoutes,
+
+  // No match
+  { path: '*', element: <Page404 /> },
+];
